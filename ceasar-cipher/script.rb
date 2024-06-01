4d1edf8884fa8aed65443a2d76d51ab22c7588ef
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-06-01 16:07:21 +0700

    Not pround

---
+++ ceasar-cipher/script.rb
@@ -0,0 +1,23 @@
+def ceasar_cipher(string, shift)
+  result = ""
+  string.each_byte do |char_num|
+    if(char_num > 64 && char_num < 91 )
+      char_num += shift
+      if(char_num > 90) 
+        result << (64 + (char_num - 90)).chr
+      elsif
+        result << char_num.chr
+      end
+    elsif (char_num > 96 && char_num < 123 )
+      char_num += shift
+      if(char_num > 122) 
+        result << (96 + (char_num - 122)).chr
+      elsif
+        result << char_num.chr
+      end
+    else
+      result << char_num.chr
+    end
+  end
+  return result
+end
